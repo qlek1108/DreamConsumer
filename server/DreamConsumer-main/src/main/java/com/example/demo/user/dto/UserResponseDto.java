ff5205493baafc2f8cc5dce11634dd71d7aaedf3
qlek1108 <qlek1108@naver.com>
2023-09-15 10:06:57 +0900

    fix

---
+++ server/DreamConsumer-main/src/main/java/com/example/demo/user/dto/UserResponseDto.java
@@ -0,0 +1,22 @@
+package com.example.demo.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserResponseDto {
+    private long userId;
+
+    private String userName;
+
+    private String email;
+
+    private String password;
+
+    private String job;
+
+    private int age;
+
+    private boolean emailAcceptance;
+}
