abc6971930be9aa95bdad48e57d0e14fd688948e
qlek1108 <qlek1108@naver.com>
2023-09-17 13:40:46 +0900

    fix

---
+++ server/DreamConsumer-main/src/main/java/com/example/demo/user/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.example.demo.user.mapper;
+
+import com.example.demo.user.dto.UserMyPageResponseDto;
+import com.example.demo.user.dto.UserPostDto;
+import com.example.demo.user.dto.UserResponseDto;
+import com.example.demo.user.entity.User;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    User UserPostDtoToUser(UserPostDto userPostDto);
+    UserResponseDto UserToUserResponseDto(User user);
+    UserMyPageResponseDto UserToUserMyPageResponseDto(User user);
+}
