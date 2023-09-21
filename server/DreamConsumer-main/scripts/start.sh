f4b2dc51dc465576d8547cfa3df2ea2c5a6de17e
qlek1108 <qlek1108@naver.com>
2023-09-21 17:57:52 +0900

    FIX

--- server/DreamConsumer-main/scripts/start.sh
+++ server/DreamConsumer-main/scripts/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 PROJECT_ROOT="/home/ubuntu/action"
-JAR_FILE="$PROJECT_ROOT/gradle-wrapper.jar"
+JAR_FILE="$PROJECT_ROOT/demo-0.0.1-SNAPSHOT.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
